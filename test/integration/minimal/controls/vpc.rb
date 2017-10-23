8c853db3ea4c7114423dae4cc07a65375e9babe7
Stephen Kuenzli <skuenzli@qualimente.com>
2017-10-23 12:49:48 -0700

    Verify basic VPC-level attributes via awsspec.

--- test/integration/minimal/controls/vpc.rb
+++ test/integration/minimal/controls/vpc.rb
@@ -4,6 +4,8 @@ require 'awsecrets'
 Awsecrets.load()
 
 vpc_name = 'test-vpc'
+env = 'test-env'
+owner = 'platform'
 
 control 'vpc' do
   describe "VPC #{vpc_name}" do
@@ -11,5 +13,12 @@ control 'vpc' do
 
     it { should exist }
     it { should be_available }
+
+    its(:cidr_block) { should eq '10.17.0.0/16' }
+
+    it { should have_tag('Name').value(vpc_name) }
+    it { should have_tag('Environment').value(env) }
+    it { should have_tag('Owner').value(owner) }
+    it { should have_tag('ManagedBy').value('Terraform') }
   end
 end
