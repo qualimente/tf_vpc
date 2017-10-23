8b82a47db487fd7f938ada010f15c733e34378c6
Stephen Kuenzli <skuenzli@qualimente.com>
2017-10-23 13:24:51 -0700

    Extract actual vpc id from tf state file and use to look-up VPC resource

--- test/integration/minimal/controls/vpc.rb
+++ test/integration/minimal/controls/vpc.rb
@@ -1,24 +1,31 @@
 require 'awspec'
 require 'awsecrets'
+require 'json'
 
 Awsecrets.load()
 
-vpc_name = 'test-vpc'
-env = 'test-env'
-owner = 'platform'
+expect_vpc_name = 'test-vpc'
+expect_env = 'test-env'
+expect_owner = 'platform'
+
+tf_state_json = json(attribute 'terraform_state', {})
+tf_module = get_current_module_from_tf_state_json(tf_state_json)
+actual_vpc_id = tf_module['outputs']['vpc.id']['value']
 
 control 'vpc' do
-  describe "VPC #{vpc_name}" do
-    subject { vpc(vpc_name) }
+
+  describe "VPC #{actual_vpc_id}" do
+    subject { vpc(actual_vpc_id) }
 
     it { should exist }
     it { should be_available }
 
     its(:cidr_block) { should eq '10.17.0.0/16' }
 
-    it { should have_tag('Name').value(vpc_name) }
-    it { should have_tag('Environment').value(env) }
-    it { should have_tag('Owner').value(owner) }
+    it { should have_tag('Name').value(expect_vpc_name) }
+    it { should have_tag('Environment').value(expect_env) }
+    it { should have_tag('Owner').value(expect_owner) }
     it { should have_tag('ManagedBy').value('Terraform') }
   end
+
 end
