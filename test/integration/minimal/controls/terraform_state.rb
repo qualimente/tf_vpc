37b3c69ccdca792f3eeae847294fabe0640e790c
Stephen Kuenzli <skuenzli@qualimente.com>
2017-10-23 13:15:47 -0700

    Extract a helper function to retrieve the module's current state so that it can be shared.

--- test/integration/minimal/controls/terraform_state.rb
+++ test/integration/minimal/controls/terraform_state.rb
@@ -1,11 +1,11 @@
 require 'json'
 require 'rspec/expectations'
 
-terraform_state = attribute 'terraform_state', {}
+require_relative 'spec_helper'
+
+tf_state_json = json(attribute 'terraform_state', {})
 
 control 'terraform_state' do
-  tf_state_json = json(terraform_state)
-
   describe 'the Terraform state file' do
     subject { tf_state_json.terraform_version }
 
@@ -16,85 +16,81 @@ control 'terraform_state' do
   describe 'the Terraform state file' do
     #require 'pry'; binding.pry; #uncomment to jump into the debugger
 
+    tf_module = get_current_module_from_tf_state_json(tf_state_json)
+    outputs = tf_module['outputs']
+    resources = tf_module['resources']
+
     # describe outputs
-    tf_state_json.modules.each { |tf_module|
-
-      if tf_module['path'].size == 2 and tf_module['path'][0] == 'root' and tf_module['path'][1] == 'it_minimal'
-        outputs = tf_module['outputs']
-        resources = tf_module['resources']
-
-        describe 'outputs' do
-          describe('VPC') do
-            describe('id') do
-              subject { outputs['vpc.id']['value'] }
-              it { is_expected.to start_with("vpc-") }
-            end
-            describe('CIDR Block') do
-              subject { outputs['vpc.cidr_block'] }
-              it { is_expected.to eq({"sensitive" => false, "type" => "string", "value" => "10.17.0.0/16"}) }
-            end
-          end
+    describe 'outputs' do
+      describe('VPC') do
+        describe('id') do
+          subject { outputs['vpc.id']['value'] }
+          it { is_expected.to start_with("vpc-") }
         end
-
-        describe 'resources' do
-          describe('VPC') do
-            vpc_primary = resources['aws_vpc.main']['primary']
-            vpc_attributes = vpc_primary['attributes']
-
-            describe('id') do
-              subject { vpc_primary['id'] }
-              it { is_expected.to eq(outputs['vpc.id']['value']) }
-            end
-
-            describe('CIDR Block') do
-              subject { vpc_attributes['cidr_block'] }
-              it { is_expected.to eq("10.17.0.0/16") }
-            end
-
-            describe('Enable ClassicLink') do
-              subject { vpc_attributes['enable_classiclink'] }
-              it { is_expected.to eq("false") }
-            end
-
-            describe('Enable DNS hostnames') do
-              subject { vpc_attributes['enable_dns_hostnames'] }
-              it { is_expected.to eq("false") }
-            end
-
-            describe('Enable DNS Support') do
-              subject { vpc_attributes['enable_dns_support'] }
-              it { is_expected.to eq("true") }
-            end
-
-            describe('Instance Tenancy') do
-              subject { vpc_attributes['instance_tenancy'] }
-              it { is_expected.to eq("default") }
-            end
-
-            describe('Tag: Name') do
-              subject { vpc_attributes['tags.Name'] }
-              it { is_expected.to eq("test-vpc") }
-            end
-
-            describe('Tag: Environment') do
-              subject { vpc_attributes['tags.Environment'] }
-              it { is_expected.to eq("test-env") }
-            end
-
-            describe('Tag: ManagedBy') do
-              subject { vpc_attributes['tags.ManagedBy'] }
-              it { is_expected.to eq("Terraform") }
-            end
-
-            describe('Tag: Owner') do
-              subject { vpc_attributes['tags.Owner'] }
-              it { is_expected.to eq("platform") }
-            end
-
-          end
+        describe('CIDR Block') do
+          subject { outputs['vpc.cidr_block'] }
+          it { is_expected.to eq({"sensitive" => false, "type" => "string", "value" => "10.17.0.0/16"}) }
         end
       end
-    }
+    end
+
+    describe 'resources' do
+      describe('VPC') do
+        vpc_primary = resources['aws_vpc.main']['primary']
+        vpc_attributes = vpc_primary['attributes']
+
+        describe('id') do
+          subject { vpc_primary['id'] }
+          it { is_expected.to eq(outputs['vpc.id']['value']) }
+        end
+
+        describe('CIDR Block') do
+          subject { vpc_attributes['cidr_block'] }
+          it { is_expected.to eq("10.17.0.0/16") }
+        end
+
+        describe('Enable ClassicLink') do
+          subject { vpc_attributes['enable_classiclink'] }
+          it { is_expected.to eq("false") }
+        end
+
+        describe('Enable DNS hostnames') do
+          subject { vpc_attributes['enable_dns_hostnames'] }
+          it { is_expected.to eq("false") }
+        end
+
+        describe('Enable DNS Support') do
+          subject { vpc_attributes['enable_dns_support'] }
+          it { is_expected.to eq("true") }
+        end
+
+        describe('Instance Tenancy') do
+          subject { vpc_attributes['instance_tenancy'] }
+          it { is_expected.to eq("default") }
+        end
+
+        describe('Tag: Name') do
+          subject { vpc_attributes['tags.Name'] }
+          it { is_expected.to eq("test-vpc") }
+        end
+
+        describe('Tag: Environment') do
+          subject { vpc_attributes['tags.Environment'] }
+          it { is_expected.to eq("test-env") }
+        end
+
+        describe('Tag: ManagedBy') do
+          subject { vpc_attributes['tags.ManagedBy'] }
+          it { is_expected.to eq("Terraform") }
+        end
+
+        describe('Tag: Owner') do
+          subject { vpc_attributes['tags.Owner'] }
+          it { is_expected.to eq("platform") }
+        end
+
+      end
+    end
     
   end
 
